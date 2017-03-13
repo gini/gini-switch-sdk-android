07f33342de499b42601002b8a907854189769857
Stephan <stephan.hagios@gmail.com>
2017-03-13 19:01:47 +0100

    more setup

--- sample/src/main/java/net/gini/tariffsdk/sample/MainActivity.java
+++ sample/src/main/java/net/gini/tariffsdk/sample/MainActivity.java
@@ -1,7 +1,9 @@
-package net.gini.ginitariffsdk;
+package net.gini.tariffsdk.sample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import net.gini.tariffsdk.R;
 
 public class MainActivity extends AppCompatActivity {
 
