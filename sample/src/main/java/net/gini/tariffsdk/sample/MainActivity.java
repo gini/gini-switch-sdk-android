f569a290e0a39adcd39c0e1b2ba1378953de91fa
Stephan <stephan.hagios@gmail.com>
2017-03-14 11:23:46 +0100

    removed not needed files from the library and extracted versions

--- sample/src/main/java/net/gini/tariffsdk/sample/MainActivity.java
+++ sample/src/main/java/net/gini/tariffsdk/sample/MainActivity.java
@@ -3,7 +3,6 @@ package net.gini.tariffsdk.sample;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import net.gini.tariffsdk.R;
 
 public class MainActivity extends AppCompatActivity {
 
