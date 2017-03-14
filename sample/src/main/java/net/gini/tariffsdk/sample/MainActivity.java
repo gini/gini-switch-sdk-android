4cd6dc210dab7e168f4a46fb8ac0711aba2a18a8
Stephan <stephan.hagios@gmail.com>
2017-03-14 15:26:53 +0100

    Added tariffsdk and activity

--- sample/src/main/java/net/gini/tariffsdk/sample/MainActivity.java
+++ sample/src/main/java/net/gini/tariffsdk/sample/MainActivity.java
@@ -1,7 +1,11 @@
 package net.gini.tariffsdk.sample;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import net.gini.tariffsdk.TariffSdk;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +14,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TariffSdk tariffSdk = TariffSdk.getSdkInstance(this);
+
+        findViewById(R.id.button_start).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View view) {
+                final Intent tariffSdkIntent = tariffSdk.getTariffSdkIntent();
+                startActivityForResult(tariffSdkIntent, TariffSdk.REQUEST_CODE);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(final int requestCode, final int resultCode,
+            final Intent data) {
+        if(requestCode == TariffSdk.REQUEST_CODE) {
+            //TODO interpret result code
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
