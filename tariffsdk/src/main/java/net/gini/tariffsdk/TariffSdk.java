17de0b2664c1be60e29492ec70a5cefa004ba526
Stephan <stephan.hagios@gmail.com>
2017-04-04 13:57:37 +0200

    PR suggestions

--- tariffsdk/src/main/java/net/gini/tariffsdk/TariffSdk.java
+++ tariffsdk/src/main/java/net/gini/tariffsdk/TariffSdk.java
@@ -7,7 +7,7 @@ import android.support.annotation.LayoutRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.StyleRes;
 
-import net.gini.tariffsdk.takepicture.TariffSdkIntentCreator;
+import net.gini.tariffsdk.takepicture.TariffSdkIntentFactory;
 
 /**
  * <p>
@@ -44,7 +44,7 @@ public class TariffSdk {
     @NonNull
     public Intent getTariffSdkIntent() {
 
-        return new TariffSdkIntentCreator(mContext, mTheme).createIntent();
+        return new TariffSdkIntentFactory(mContext, mTheme).createIntent();
     }
 
     /**
