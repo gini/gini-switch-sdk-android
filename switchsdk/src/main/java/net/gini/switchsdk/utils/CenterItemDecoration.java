33416be383302a86b1fa4238e2d0f379ef766ba2
Stephan <stephan.hagios@gmail.com>
2017-07-24 12:08:45 +0200

    Moved margin of items into the item decorator

--- switchsdk/src/main/java/net/gini/switchsdk/utils/CenterItemDecoration.java
+++ switchsdk/src/main/java/net/gini/switchsdk/utils/CenterItemDecoration.java
@@ -3,6 +3,7 @@ package net.gini.switchsdk.utils;
 
 import android.graphics.Rect;
 import android.support.v7.widget.RecyclerView;
+import android.util.TypedValue;
 import android.view.View;
 
 public class CenterItemDecoration extends RecyclerView.ItemDecoration {
@@ -11,14 +12,17 @@ public class CenterItemDecoration extends RecyclerView.ItemDecoration {
     public void getItemOffsets(final Rect outRect, final View view, final RecyclerView parent,
             final RecyclerView.State state) {
 
+        final int margin = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 4,
+                view.getContext().getResources().getDisplayMetrics());
+
         final int position = parent.getChildAdapterPosition(view);
         final int itemCount = parent.getAdapter().getItemCount();
 
         final int totalWidth = Math.max(parent.getMeasuredWidth(), parent.getWidth());
         final int cardWidth = Math.max(view.getLayoutParams().width, view.getWidth());
         view.getMeasuredWidth();
-        int left = 0;
-        int right = 0;
+        int left = margin;
+        int right = margin;
 
         //first item -> First and last can be the same
         if (position == 0) {
@@ -28,7 +32,7 @@ public class CenterItemDecoration extends RecyclerView.ItemDecoration {
         if (position == itemCount - 1) {
             right = (totalWidth - cardWidth) / 2;
         }
-        outRect.set(left, 0, right, 0);
+        outRect.set(left, margin, right, margin);
     }
 }
 
